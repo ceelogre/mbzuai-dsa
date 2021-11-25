32697cc154adb8c26ec22909c15ef578188a3668
ceelogre <sam.nishimwe@yahoo.com>
2021-11-25 14:40:46 +0200

    ch: 2d array questions

---
+++ array_sum.py
@@ -0,0 +1,8 @@
+# given a 2d array, calculate the sum of all the numbers in the array
+# example:
+# sum_of_array([[1,2,3],[4,5,6]]) => 21
+
+
+#given a 2d array, return a list of pairs of numbers that add up to the target value.
+#example:
+#return_sum([[1,2,3],[4,5,6]], 7) => [[2,5],[1,6], [3,4]]
