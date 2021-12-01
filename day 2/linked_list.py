830aaecb867370af270e9fc87883f6925236b2e6
ceelogre <sam.nishimwe@yahoo.com>
2021-12-01 11:14:21 +0200

    fix: traverse fix

--- day 2/linked_list.py
+++ day 2/linked_list.py
@@ -15,21 +15,32 @@ class Linked_list:
   def insert_at_start(first_variable, node):
     first_variable.head = node
   def insert_at_end(first_variable, new_node):
-    while first_variable.head != None:
-      first_variable.head = first_variable.head.next
-    first_variable.head = new_node
-  def insert():
+    current_node = first_variable.head
+    while current_node.next != None:
+      current_node = current_node.next
+    current_node.next = new_node
+
+  def insert(self, pos, newest_node):
+    for i in range(2, pos):
+      if self.head != None:
+        self.head = self.head.next
+    newest_node.next = self.head.next
+    self.head.next = newest_node
     pass
   
   def traverse(first_variable):
-    while first_variable.head != None:
-      print(first_variable.head.data)
-      first_variable.head = first_variable.head.next
-
+    current_node = first_variable.head
+    while current_node != None:
+      print(current_node.data)
+      current_node = current_node.next
+    
 my_linked_list = Linked_list()
 my_linked_list.insert_at_start(first)
 my_linked_list.traverse()
 
 last = Node('last')
 my_linked_list.insert_at_end(last)
+my_linked_list.traverse()
+
+my_linked_list.insert(2, Node('middle'))
 my_linked_list.traverse()
