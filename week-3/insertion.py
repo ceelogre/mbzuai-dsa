dfd86ac6279eaa269930a3d0761b7c7308dfaf33
ceelogre <sam.nishimwe@yahoo.com>
2021-12-08 12:36:11 +0200

    ch: add insertion sort stub

---
+++ week-3/insertion.py
@@ -0,0 +1,11 @@
+# sort the list by using insertion sort algorithm
+# use these resources: 
+# https://www.freecodecamp.org/news/insertion-sort-what-it-is-and-how-it-works/
+# https://www.programiz.com/dsa/insertion-sort
+# 
+
+def insertion_sort(L):
+  pass
+
+L = [43, 93, 96, 6,34,78,5, 43]
+# print(insertion_sort(L)) = > [5, 6, 34, 43, 43, 78, 93, 96]
