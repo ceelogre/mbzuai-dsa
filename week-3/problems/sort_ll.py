0174ca5728d08c1b642fb0aeeef7be8bc3943c04
ceelogre <sam.nishimwe@yahoo.com>
2021-12-11 23:11:16 +0200

    feat: sort linked list

---
+++ week-3/problems/sort_ll.py
@@ -0,0 +1,9 @@
+from linkedlist import Singly_linkedlist
+
+train = Singly_linkedlist()
+train.insert(1, 'one')
+train.insert(1, 'zero')
+train.insert(1, 'middle')
+train.insert(2, 'arena')
+train.sort()
+train.display()
